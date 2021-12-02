e4d4e57f703b2a9e485e7e399130405258d6b3fd
yuminbc <spdlxm9666@nate.com>
2021-12-02 14:46:50 +0900

    final

--- src/routes/word/index.js
+++ src/routes/word/index.js
@@ -1,22 +1,30 @@
 const express = require('express')
-const Word = require('../../models/Word')
 const WordRouter = express.Router()
 
-const word = require('../../models/Word') // word 가져옴
+const Word = require('../../models/Word')
+
+
+// const word = require('../../models/Word') // word 가져옴
   
 //처리로직
 // /api/word/ , /api/words/학원 //(파라미터)
-WordRouter.route('/(:word)?').get(async(req, res) => {
+WordRouter.route('/(:option?)/(:word)?').get(async(req, res) => {
     let words = []
-    const {word} = req.params 
+    const {word} = req.params
+    const {option} = req.params
+
+    // const queries = word.split(',')
+    // console.log(queries)
     console.log(req.params.word)
     // res.send(word)
  
     // 사용자로부터 쿼리가 존재하는 경우
     // db.collection.find({r_word: word}) // 쿼리로 DB검색
-    if(word !== "undefined" && word !== undefined){ // 데이터베이스에서 쿼리 단어를 검색
+    // if(word !== "null" && word !== null){ // 데이터베이스에서 쿼리 단어를 검색
+    if(word !== "undefined" && word !== undefined){
         // res.json({status:200, msg:'특정 단어 검색'})
         console.log(word)
+        console.log(option)
         
         try{
             console.log('단어 쿼리...')
@@ -26,25 +34,36 @@ WordRouter.route('/(:word)?').get(async(req, res) => {
             // ]}})
             // words = await Word.find({ r_word: { $regex: `^${word}`}})//데이터 베이스에서 검색어로 시작하는 단어 검색
             // words = await Word.find({ r_word: { $regex: `${word}$`}})// 끝나는 단어로 검색
+            if(option === 'word'){
             words = await Word.find({ r_word: { $regex: `${word}`}})//포함하는 단어 검색
-            // words = await Word.find({ r_des: { $regex: `${word}`}}) //설명 부분에서 포함하는단어 검색
-            // words = await Word.find({$or:
-            //     [
-            //     {r_word: {$regex:`${word}`}},
-            //     {r_des: {$regex:`${word}`}}
-            //     ]
-            // })
-            .sort({"r_word": 1})//-1 최신순(내림차순) 1과거순 (오름차순)
+            .sort({"r_word": 1})
+            }
+            else if(option === 'des' ){
+            words = await Word.find({ r_des: { $regex: `${word}`}}) //설명 부분에서 포함하는단어 검색
+            .sort({"r_word": 1})
+            }
+
+            else if(option === 'worddes'){
+            words = await Word.find({$or:    // 단어 내용 검색
+                [
+                {r_word: {$regex:`${word}`}},
+                {r_des: {$regex:`${word}`}},
+                ]
+            }).sort({"r_word": 1})}
+            
+
+            
+            //.sort({"r_word": 1})} -1 최신순(내림차순) 1과거순 (오름차순)
             // .limit(6)//6개만 검색
         }catch(e){
             console.log(e)
         }
 
-    } else { // 데이터베이스에서 전체 단어 검색
-        // res.json({status:200, msg:'전체 단어 검색'})
-        // console.log(word)
+    } else if (word == "null"|| word == null) { // 검색어가 존재하지 않는 경우 데이터베이스에서 전체 단어 검색
+        res.json({status:200, msg:'전체 단어 검색'})
+        console.log(word)
         words.html = ["no query"]
-        // console.log(`word database: ${Word}`)//콘솔에 표시
+        console.log(`word database: ${Word}`)//콘솔에 표시
         try{
         words = await Word.find()
         }catch(e){
@@ -56,8 +75,3 @@ WordRouter.route('/(:word)?').get(async(req, res) => {
 
 
 module.exports = WordRouter
-
-
-
-
- 
